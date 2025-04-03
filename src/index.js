9d1530ed5e9533cd4343bead14387976cb214957
LucasUmada <iuderas@outlook.com>
2025-04-03 10:51:46 -0300

    fix: fix demo implantation via API

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
-import { getCookieSzb } from './modules/cookie'
+import { getCookie, createSID } from './modules/cookie'
 import { renderButtons } from './modules/buttons'
-import { getProduct, createSID } from './modules/fetcher'
+import { getProduct } from './modules/fetcher'
 import { renderRecommendation } from './modules/recommendation'
 
-const productLink = 'https://store.sizebay.com/products/nylon-bomber-jacket-in-stone'
+const productLink = 'https://store.sizebay.com/products/8522955751575'
 const tenantId = 1039
 const lang = 'en'
 
-const init = async () => {
-  const SID = getCookieSzb('SIZEBAY_SESSION_ID_V4') ? getCookieSzb('SIZEBAY_SESSION_ID_V4') : await createSID()
+export const init = async () => {
+  const SID = getCookie('SIZEBAY_SESSION_ID_V4') ? getCookie('SIZEBAY_SESSION_ID_V4') : await createSID()
 
-  const { id, accessory } = await getProduct(SID, productLink)
+  const { id, accessory } = await getProduct(productLink, {
+    permalink: productLink,
+    tenantId,
+  })
+
+  console.log({ id, accessory })
 
   if (id) {
     renderButtons(id, accessory, SID, tenantId, lang)
@@ -18,4 +23,4 @@ const init = async () => {
   }
 }
 
-init()
+window.init = init
--- src/modules/fetcher.js
+++ src/modules/fetcher.js
@@ -1,27 +1,83 @@
-import { setCookie } from './cookie'
+import { getCookie, createSID } from './cookie'
 import { APP_URL } from './constants'
 
-const fetcher = async (url) => {
-  const res = await fetch(`${APP_URL}${url}`, {
-    method: 'GET',
-  })
+export default async function fetcher(URL, options) {
+  const parsedOptions = {
+    ...options,
+    headers: {
+      ...options?.headers,
+      referer: options?.permalink,
+      tenant_id: options?.tenant_id,
+      device: 'desktop',
+      'x-szb-country': null,
+      'x-szb-tenant-id': options?.tenant_id,
+      'x-szb-device': 'desktop',
+    },
+  }
 
-  if (res?.ok) return await res.json()
+  try {
+    const res = await fetch(URL, {
+      credentials: options?.credentials || 'include',
+      referrerPolicy: 'origin-when-cross-origin',
+      ...parsedOptions,
+    })
 
-  console.error('Product not found')
-  return
+    const hasResponse = res.status !== 500 && res.status !== 404
+    const hasJsonResponse = !!res.headers.get('content-type')?.includes('application/json')
+
+    const parsedResponse = hasJsonResponse ? await res.json() : await res.text()
+
+    if (hasResponse) {
+      return {
+        ...parsedResponse,
+        message: 'Success',
+        status: res.status,
+      }
+    }
+
+    return {
+      error: true,
+      message: typeof parsedResponse === 'string' ? parsedResponse : DEFAULT_ERROR_MESSAGE,
+      status: res.status,
+    }
+  } catch (err) {
+    logger(`An error ocurred while fetching data from ${URL}.`)
+
+    return {
+      error: true,
+      message: DEFAULT_ERROR_MESSAGE,
+      status: 500,
+    }
+  }
 }
 
-export const createSID = async () => {
-  const szbSID = await fetcher('api/me/session-id')
-  setCookie('SIZEBAY_SESSION_ID_V4', szbSID)
-  return szbSID
-}
+const SID = getCookie('SIZEBAY_SESSION_ID_V4') || (await createSID())
 
-export const getProduct = async (SID, permalink) => {
-  const getProd = await fetcher(`plugin/my-product-id?sid=${SID}&permalink=${permalink}`)
+export const getProduct = async (permalink, tenantId) => {
+  const URL = `${APP_URL}plugin/my-product-id?sid=${SID}&permalink=${permalink}`
 
-  return getProd
+  let product = null
+
+  const data = await fetcher(URL)
+
+  const foundProduct = !data.error && data.status !== 500 && data.message.status !== 404
+  const productIsPending = data.status === 404 && !data.message.includes('Not product found')
+
+  if (foundProduct) {
+    product = {
+      clothesType: data.clothesType || null,
+      accessory: data.accessory,
+      mixMatch: data.mixMatch,
+      id: data.id,
+      shoe: data.shoe,
+    }
+  } else if (productIsPending) {
+    product = {
+      isPending: true,
+    }
+  }
+
+  return product
 }
 
 export const getRecommendation = async (productId, SID, tenantId) => {
